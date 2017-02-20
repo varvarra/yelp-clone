c5c83922d0ea7c9d277ae421208ab5f31855b162
Varvara Shinkarenko <shinkarenko.varvara@gmail.com>
2017-02-20 17:44:00 +0000

    Created a form to add a restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,4 +4,18 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  private
+
+  def restaurant_params
+     params.require(:restaurant).permit(:name)
+  end
 end
