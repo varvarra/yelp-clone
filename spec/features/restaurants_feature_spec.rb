845aa2f3c4b7dafeed1ad76bbad092c2e4f7d784
Stefan Liute <stefanliute@gmail.com>
2017-02-20 17:04:40 +0000

    Add a restaurant model

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -8,4 +8,16 @@ feature 'restaurants' do
       expect(page).to have_link 'Add a restaurant'
     end
   end
+
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create(name: 'Los Pollos Hermanos')
+    end
+
+    scenario 'display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content 'Los Pollos Hermanos'
+      expect(page).not_to have_content 'No restaurants yet'
+    end
+  end
 end
